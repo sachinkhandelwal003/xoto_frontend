9bb731cc10dac7de1e4e27b0933fcfe0cba26e70
manjeet meghwanshi <manjeetkatariya2000@gmail>
2025-11-12 16:41:03 +0530

    new commit

--- src/components/homepage/About.jsx
+++ src/components/homepage/About.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import Powered from '../../components/aboutt/Powered.jsx'
-import Second from '../../components/aboutt/Second.jsx'
+import Powered from '../../components/Aboutt/Powered'
+import Second from '../../components/aboutt/Second'
 // import Vision from '../../components/aboutt/Vision.jsx'
-import Mission from '../../components/aboutt/Mission.jsx'
+import Mission from '../../components/aboutt/Mission'
 // import Dna from '../../components/aboutt/Dna.jsx'
 // import { SecurityRounded } from '@mui/icons-material'
 
