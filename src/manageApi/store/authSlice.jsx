3f5458ee1a2ff4199db02a5b2d26038e82803ec3
Sachin Khandelwal <business.kotiboxglobaltech@gmail.com>
2025-12-04 10:03:32 +0530

    new commit

--- src/manageApi/store/authSlice.jsx
+++ src/manageApi/store/authSlice.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 
 // Set base URL globally
 const API_BASE = 'https://kotiboxglobaltech.online/api';
-const API_BASE = 'http://localhost:5000/api';
+// const API_BASE = 'http://localhost:5000/api';
 
 // Load from localStorage
 const loadInitialState = () => {
