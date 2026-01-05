d35977c5ac4906be565ac5f53f595faf58faea44
Sachin Khandelwal <business.kotiboxglobaltech@gmail.com>
2026-01-05 12:49:53 +0530

    partners_logo

--- src/components/BuyRent/Buy4.jsx
+++ src/components/BuyRent/Buy4.jsx
@@ -5,30 +5,35 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 import "swiper/css";
 import { useTranslation } from "react-i18next";
-
+import partner1 from '../../assets/xoto_partners/xoto_logo1.png';
+import partner2 from '../../assets/xoto_partners/xoto_logo2.png';
+import partner3 from '../../assets/xoto_partners/xoto_logo3.png';
+import partner4 from '../../assets/xoto_partners/xoto_logo4.png';
+import partner5 from '../../assets/xoto_partners/xoto_logo5.png';
+import partner6 from '../../assets/xoto_partners/xoto_logo6.png';
+import partner7 from '../../assets/xoto_partners/xoto_logo7.png';
+import partner8 from '../../assets/xoto_partners/xoto_logo8.png';
+import partner9 from '../../assets/xoto_partners/xoto_logo9.png';
+import partner10 from '../../assets/xoto_partners/xoto_logo10.png';
+import partner11 from '../../assets/xoto_partners/xoto_logo11.png';
 import waveint5 from "../../assets/img/wave/waveint5.png";
-// import company1 from "../../assets/img/home/companylogo1.png";
-// import company2 from "../../assets/img/home/companylogo2.png";
-// import company3 from "../../assets/img/home/companylogo3.png";
 
 export default function TrustPresenceSection() {
-  const { t } = useTranslation("buy4"); // 👈 namespace
+  const { t } = useTranslation("buy4");
 
+  // Define the logos array
   const logos = [
-    { icon: "https://xotostaging.s3.me-central-1.amazonaws.com/properties/1767441852149-Screenshot%202026-01-03%20172246.png" },
-    { icon: "https://xotostaging.s3.me-central-1.amazonaws.com/properties/1767441895828-Screenshot%202026-01-03%20172336.png" },
-    { icon: "https://xotostaging.s3.me-central-1.amazonaws.com/properties/1767441942079-Screenshot%202026-01-03%20172436.png" },
-    { icon: "https://xotostaging.s3.me-central-1.amazonaws.com/properties/1767441959428-Screenshot%202026-01-03%20172532.png" },
-    { icon: "https://xotostaging.s3.me-central-1.amazonaws.com/properties/1767442087630-Screenshot%202026-01-03%20172639.png" },
-    { icon: "https://xotostaging.s3.me-central-1.amazonaws.com/properties/1767442118961-Screenshot%202026-01-03%20172703.png" },
-    { icon: "https://xotostaging.s3.me-central-1.amazonaws.com/properties/1767442157517-Screenshot%202026-01-03%20172731.png" },
-    { icon: "https://xotostaging.s3.me-central-1.amazonaws.com/properties/1767442211857-Screenshot%202026-01-03%20172849.png" },
-    { icon: "https://xotostaging.s3.me-central-1.amazonaws.com/properties/1767442254795-Screenshot%202026-01-03%20172951.png" },
-    { icon: "https://xotostaging.s3.me-central-1.amazonaws.com/properties/1767442321079-Screenshot%202026-01-03%20173017.png" },
-    { icon: "https://xotostaging.s3.me-central-1.amazonaws.com/properties/1767442352175-Screenshot%202026-01-03%20173040.png" },
-    { icon: "https://xotostaging.s3.me-central-1.amazonaws.com/properties/1767442445482-Screenshot%202026-01-03%20173113.png" },
-    { icon: "https://xotostaging.s3.me-central-1.amazonaws.com/properties/1767442525434-Screenshot%202026-01-03%20173140.png" },
-    { icon: "https://xotostaging.s3.me-central-1.amazonaws.com/properties/1767442584681-Screenshot%202026-01-03%20173242.png" },
+    { icon: partner1 },
+    { icon: partner2 },
+    { icon: partner3 },
+    { icon: partner4 },
+    { icon: partner5 },
+    { icon: partner6 },
+    { icon: partner7 },
+    { icon: partner8 },
+    { icon: partner9 },
+    { icon: partner10 },
+    { icon: partner11 },
   ];
 
   return (
@@ -48,14 +53,18 @@ export default function TrustPresenceSection() {
       </h2>
 
       {/* Swiper */}
-      <div className="relative w-screen -mx-[calc((100vw-100%)/2)] mb-16 md:mb-20 ">
+      <div className="relative w-screen -mx-[calc((100vw-100%)/2)] mb-16 md:mb-20">
         <Swiper
           modules={[Autoplay]}
           slidesPerView={7}
           spaceBetween={40}
           loop
           speed={3000}
-          autoplay={{ disableOnInteraction: false, reverseDirection: true }}
+          autoplay={{ 
+            delay: 0,
+            disableOnInteraction: false, 
+            reverseDirection: true 
+          }}
           centeredSlides
           className="!overflow-visible"
         >
@@ -66,26 +75,35 @@ export default function TrustPresenceSection() {
             >
               <div
                 className="
-                  relative group bg-[var(--color-body)] cursor-pointer
-                  w-28 h-28 sm:w-36 sm:h-36 md:w-40 md:h-40 lg:w-48 lg:h-48
-                  rounded-full border border-green-300
+                  relative group bg-white cursor-pointer
+                  w-32 h-32 sm:w-36 sm:h-36 md:w-40 md:h-40 lg:w-44 lg:h-44
+                  rounded-full border-2 border-green-300
                   flex items-center justify-center
-                  shadow-xl
+                  shadow-xl shadow-green-100/50
                   transition-all duration-300 ease-out
-                  hover:scale-125
+                  hover:scale-125 hover:shadow-2xl hover:shadow-green-200/50
+                  overflow-hidden
                 "
               >
-            <img
-            src={logo.icon}
-            alt="Logo"
-            className="
-              w-full h-full
-              object-fit-contain
-              rounded-full
-              transition-all duration-300
-              group-hover:scale-110
-            "
-/>
+                {/* Logo Container - Perfect circle with padding */}
+                <div className="
+                  w-[70%] h-[70%]
+                  flex items-center justify-center
+                  overflow-hidden
+                  rounded-full
+                  p-3
+                ">
+                  <img
+                    src={logo.icon}
+                    alt={`Partner Logo ${index % logos.length + 1}`}
+                    className="
+                      w-full h-full
+                      object-contain
+                      transition-all duration-300
+                      group-hover:scale-110
+                    "
+                  />
+                </div>
               </div>
             </SwiperSlide>
           ))}
